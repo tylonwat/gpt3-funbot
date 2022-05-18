94c1e3fa70929829671d9cc8f1312bea83716aa8
tylonwat <55420827+tylonwat@users.noreply.github.com>
2022-05-18 00:51:36 -0400

    first commit
    code to set up DONDA bot

---
+++ dondabot.py
@@ -0,0 +1,35 @@
+from tkinter.messagebox import QUESTION
+from dotenv import load_dotenv
+from random import choice
+from flask import Flask, request
+import os
+import openai
+
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+completion = openai.Completion()
+
+
+
+start_sequence = "User:"
+restart_sequence = "DONDA:"
+
+def ask(question, chat_log=None):
+    prompt_text = f'{chat_log}{restart_sequence}:{question}{start_sequence}:'
+    response = openai.Completion.create(
+    engine="text-curie-001",
+    prompt= prompt_text,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    stop=["/n"]
+)
+story = response['choices'][0]['text']
+ return str(story)
+
+ def append_interaction_to_chat_log(question, answer, chat_log=None):
+     if chat_log is None: 
+         chat_log = session_prompt 
+         return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}
